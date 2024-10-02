fe81689876e775c14471d9373084ec6b43cb78b1
Lahiru Nanayakkara <lahirunanayakkara924@gmail.com>
2024-10-02 19:35:43 +0530

    Ready to deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target:'http://localhost:3000',
-        secure:false
-      }
-    }
+      "/api": {
+        target: import.meta.env.VITE_BACKEND_URL,
+        secure: false,
+      },
+    },
   },
   plugins: [react()],
-})
+});
